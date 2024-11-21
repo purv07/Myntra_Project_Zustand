30d9ea7486d352d114003b559e9ea8b15a031ed3
purv07 <73741156+purv07@users.noreply.github.com>
2024-11-21 17:20:16 +0530

    Update Cart.jsx

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -14,7 +14,7 @@ export default function Cart() {
           <div className='flex justify-center flex-col h-screen '>
             <p className='font-Cust_NavbarFont font-bold mx-auto text-[20px] uppercase'>YOUR Cart IS EMPTY</p>
             <p className='font-Cust_NavbarFont font-normal  text-center text-[18px] text-[#94989f] mx-auto mt-5 max-w-[360px]'>Add items that you like to Buy.Review them anytime and easily move them to the Wishlist.</p>
-            <img src='https://raw.githubusercontent.com/purv07/Myntra_Project_Zustand/refs/heads/main/src/assets/SVG/Add%20notes-amico.svg' className='mx-auto w-[200px] h-[200px]' />
+            <img src='https://raw.githubusercontent.com/purv07/Myntra_Project_Zustand/refs/heads/main/src/assets/SVG/Add%20notes-pana.svg' className='mx-auto w-[200px] h-[200px]' />
             <div onClick={() => { NavigateToHome('/') }} className='cursor-pointer border border-[#1D85FC] px-10 py-3 mx-auto rounded-[3px] mt-5 text-[#1D85FC] font-bold'>CONTINUE SHOPPING</div>
           </div>
           :
